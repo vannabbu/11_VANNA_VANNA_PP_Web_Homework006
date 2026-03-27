35da855f54cfa2c03ff3d07299acbac1a87ba65e
NamVanna <namvanna62@gmail.com>
2026-03-27 20:11:36 +0700

    Finalizing product and customer list components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { UserHeader } from "../components/user-header"
 import "./globals.css"
-// src/app/layout.js
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar" // Added SidebarTrigger here
+
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"    
 
 export default function RootLayout({ children }) {
@@ -14,7 +14,6 @@ export default function RootLayout({ children }) {
           <main className="flex-1 flex flex-col bg-slate-50/50">
             <header className="flex h-16 shrink-0 items-center justify-between border-b bg-white px-4">
               <div className="flex items-center gap-2">
-                {/* Now this will work because it is imported above */}
                 <SidebarTrigger /> 
               </div>
               <UserHeader />
